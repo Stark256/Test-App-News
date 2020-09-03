62e106cb0de1ce6cb35776faeb8afa1087cc6600
Ihor <starkhyperpro@gmail.com>
2020-09-03 15:41:57 +0300

    init app

--- app/src/main/java/com/features/testapp_news/app/MainActivity.kt
+++ app/src/main/java/com/features/testapp_news/app/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.features.testapp_news
+package com.features.testapp_news.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.features.testapp_news.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
